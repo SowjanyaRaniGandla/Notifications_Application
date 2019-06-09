9b26a9db1c8f4b0e180ab6c7986b6899c34b275c
SowjanyaRaniGandla <39415742+SowjanyaRaniGandla@users.noreply.github.com>
2019-06-09 11:58:00 +0530

    Update notification.py

--- notification.py
+++ notification.py
@@ -11,9 +11,8 @@ def checkNotifications():
     flag = 0
     for line in fileName: 
         if today in line: 
-            line = line.split(' ') 
             flag =1
-            os.system('notify-send "Notifications for Today: ' + line[1] + ' ' + line[2] + ' ' + line[3] + '"') 
+            os.system('notify-send "Notifications for Today: ' +line+'"') 
     if flag == 0: 
             os.system('notify-send "No Notifications for Today!"') 
 
